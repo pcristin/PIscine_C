99c058f8a8a95697e00d0d26a1ff96149334edcb
Oleg <ozaytsev09@gmail.com>
2019-02-17 21:07:57 +0300

    Header file с основными функциями

---
+++ school21/header.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+# define HEADER_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+void    ft_putchar(char c);
+void    ft_putnbr(int nb);
+void    ft_putstr(char *str);
+#endif
