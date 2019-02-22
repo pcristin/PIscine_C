6e24ab6ac0076da9db207907413555ac4a2202c1
Oleg <ozaytsev09@gmail.com>
2019-02-22 20:35:35 +0300

    Rep tolko dlya src school 21

--- school21/header.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER_H
-# define HEADER_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-void    ft_putchar(char c);
-void    ft_putnbr(int nb);
-void    ft_putstr(char *str);
-#endif
